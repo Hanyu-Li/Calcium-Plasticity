f0cda322f9fde42aeb92d13256418faa50ac940f
Hanyu Li <hanyu@hodgkin.bsd.uchicago.edu>
2016-02-23 15:26:22 -0600

    added subplots

--- core_debug.py
+++ core_debug.py
@@ -1,20 +1,13 @@
 
-# coding: utf-8
-
-# In[1]:
-
-
-
-
-# In[ ]:
 
 from brian2 import *
 import csv
 import pylab
+from subprocess import call
 
 #get_ipython().magic(u'matplotlib inline')
 
-def visualise_connectivity(S):
+def visualize_connectivity(S):
     Ns = len(S.source)
     Nt = len(S.target)
     figure(1, figsize=(10, 4))
@@ -33,20 +26,95 @@ def visualise_connectivity(S):
     ylim(-1, Nt)
     xlabel('Source neuron index')
     ylabel('Target neuron index')
-def visualize_IF_curve(I=None, F=None):
+def visualize_all(I=None, F=None, rho=None, t=None, resets=None):
+    fig = figure(figsize=(50, 30))
+    plot_num = 4
+
+    ax1 = fig.add_subplot(plot_num, 1, 1)
+    visualize_tI_curve(I, t, sub=True,ax=ax1)
+
+    ax2 = fig.add_subplot(plot_num, 1, 2)
+    visualize_IF_curve(I, F, t, sub=True, ax=ax2)
+
+    ax3 = fig.add_subplot(plot_num, 1, 3)
+    ax4 = fig.add_subplot(plot_num, 1, 4)
+    visualize_F_rho_curve(F, rho, I, t, resets, sub=True, ax_a=ax3, ax_b=ax4)
+
+    draw()
+    savefig('results/all.png')
+
+
+def visualize_tI_curve(I=None, t=None, sub=False, ax=None):
+    if not sub:
+        figure(figsize=(10,8))
+        ax = plt.gca()
+    title('I ext')
+    print t.shape, I[:,0].shape
+    ax.plot(t, I[:,0])
+
+def visualize_IF_curve(I=None, F=None, t=None, sub=False, ax=None):
     interp = F.shape[0] / I.shape[0]
     if len(F.shape) == 1:
         sample = 1
     else:
         sample = F.shape[1]
 
-    avg_F = np.ndarray((I.shape[0], sample), dtype=float64)
-    print I.shape[0], F.shape[0], interp, sample
-    figure(figsize=(10,8))
+    a = F.reshape((-1,10,sample))
+    avg_F = np.mean(a,axis=1)
+    
+    if not sub:
+        figure(figsize=(10,8))
+        ax = plt.gca()
+    title('IF_curve')
+    print t.shape
     for i in arange(sample):
-        avg_F[:,i] = np.mean(F[:,i].reshape(-1, interp), axis=1)
-        print I.shape, avg_F.shape
-        plot(I, avg_F)
+        #avg_F[:,i] = np.mean(F[:,i].reshape(-1, interp), axis=1)
+        #print I.shape, avg_F.shape
+        print t.shape, avg_F[:,i].shape
+        #plot(I[:,0], avg_F[:,i])
+        label = 'sigma = '+str(5*(i+1))
+        ax.plot(t, avg_F[:,i], label=label)
+    ax.legend()
+    #savefig('results/e.png')
+    draw()
+def visualize_F_rho_curve(F=None, rho=None, I=None, t=None, resets=None, sub=False, ax_a=None, ax_b=None):
+    if len(F.shape) == 1:
+        sample = 1
+    else:
+        sample = F.shape[1]
+    a = F.reshape((-1,10,sample))
+    avg_F = np.ndarray((F.shape[0]/10, sample), dtype=float64)
+    avg_F = np.mean(a, axis=1)
+    #avg_rho = np.ndarray((avg_F.shape[0], sample), dtype=float64)
+    '''
+    if rho.shape[0] == 1:
+        avg_rho = rho
+    else:
+        avg_rho = np.mean(rho, axis=0)
+    '''
+    avg_rho = np.mean(rho, axis=0)
+    if not sub:
+        figure(figsize=(10,8))
+        ax_a = plt.gca()
+    ax_a.set_title('t rho curve')
+    print avg_F.shape, avg_rho.shape
+    for i in arange(sample):
+        ax_a.plot(t, avg_rho[:,i])
+        #plot(avg_F[:,i], avg_rho[:,i])
+
+
+    #if resets > 1:
+    avg_avg_rho = np.mean(avg_rho.reshape((resets, -1, sample)), axis=1)
+    print "avgavg", avg_avg_rho.shape
+    if not sub:
+        figure(figsize=(10,8))
+        ax_b = plt.gca()
+    ax_b.set_title('average rho curve')
+    for i in arange(sample):
+        label = 'sigma = '+str(5*(i+1))
+        ax_b.plot(avg_avg_rho[:,i], label=label)
+    ax_b.legend()
+    #savefig('results/f.png')
     draw()
         
 
@@ -78,10 +146,15 @@ class Brian_Simulator:
         self.params = params
         self.debug = debug
 
-    def run(self, param_diffs):
-        #set_device('cpp_standalone')
-        prefs.codegen.target = 'cython'
-        #prefs.devices.cpp_standalone.openmp_threads = 8
+    def run(self, param_diffs, mode='cython', resets=1, cpp_directory='output_0'):
+        ## cpp mode
+        if mode == 'cpp_standalone':
+            set_device('cpp_standalone')
+            #prefs.devices.cpp_standalone.openmp_threads = 8
+        elif mode == 'cython':
+        ## cython mode
+            prefs.codegen.target = 'cython'
+
         start_scope()
 
         #control parameters
@@ -157,7 +230,7 @@ class Brian_Simulator:
 
         # use convention S_[to][from]
 
-        S_EE = Synapses(G_E, G_E, model = synaptic_model_plastic, pre = pre_model_E_plastic, post = post_model_E_plastic)
+        S_EE = Synapses(G_E, G_E, model = synaptic_model_plastic, pre = pre_model_E_static, post = post_model_E_plastic)
         S_IE = Synapses(G_E, G_I, model = synaptic_model_static, pre = pre_model_E_static, post = None)
         S_EI = Synapses(G_I, G_E, model = synaptic_model_static, pre = pre_model_I_static, post = None)
         S_II = Synapses(G_I, G_I, model = synaptic_model_static, pre = pre_model_I_static, post = None)
@@ -198,13 +271,48 @@ class Brian_Simulator:
         spikemon_G_I = SpikeMonitor(G_I)
         popratemon_G_E = PopulationRateMonitor(G_E)
         popratemon_G_I = PopulationRateMonitor(G_I)
-        run(self.simulation_length*ms, report='stdout', report_period=1*second)
-        #run((self.simulation_length-observe_window)*ms, report='stdout', report_period=1*second)
 
-        # Recorded simulation
-        #run(observe_window*ms, report='stdout', report_period=1*second)
 
-        #device.build(directory='output', compile=True,clear=True, run=True, debug=False)
+        if mode == 'cython':
+            if resets > 1:
+                for r in arange(resets):
+                    #run(self.simulation_length/resets*ms, report='stdout', report_period=10*second)
+                    run(self.simulation_length/resets*ms)
+                    print double(r)/double(resets) * 100, '%'
+                    S_EE.cpre = cpre_0
+                    S_EE.cpost= cpost_0
+                    S_EE.rho = rho_0
+            else:
+                run(self.simulation_length*ms)
+                    
+            #run((self.simulation_length-observe_window)*ms, report='stdout', report_period=1*second)
+
+            # Recorded simulation
+            #run(observe_window*ms, report='stdout', report_period=1*second)
+
+        if mode == 'cpp_standalone':
+            if resets > 1:
+                device.insert_code('main', '''
+                    int _i = 0;
+                    for(_i=%s;_i>0;_i--){
+                        cout << _i << endl;
+                    ''' % (resets))
+                #for r in arange(resets):
+                    #run(self.simulation_length/resets*ms, report='stdout', report_period=10*second)
+                run(self.simulation_length/resets*ms)
+
+                #print double(r)/double(resets) * 100, '%'
+                S_EE.cpre = cpre_0
+                S_EE.cpost= cpost_0
+                S_EE.rho = rho_0
+                device.insert_code('main','''
+                    }
+                ''')
+                    
+
+            else:
+                run(self.simulation_length*ms)
+            device.build(directory=cpp_directory, compile=True, run=True, debug=False)
 
 
         # Simulation ends
@@ -246,14 +354,16 @@ class Brian_Simulator:
             figure(figsize=std_size)
             subplot(211)
             title('excitatory spike rate')
-            plot(popratemon_G_E.t[window_length-1:]/ms, binned_rate_E)
+            #plot(popratemon_G_E.t[window_length-1:]/ms, binned_rate_E)
+            plot(popratemon_G_E.t/ms, binned_rate_E)
 
             #plot(self.I_ext_E[:,0], binned_rate) 
             
             subplot(212)
             title('inhibitory spike rate')
 
-            plot(popratemon_G_I.t[window_length-1:]/ms, binned_rate_I)
+            #plot(popratemon_G_I.t[window_length-1:]/ms, binned_rate_I)
+            plot(popratemon_G_I.t/ms, binned_rate_I)
             savefig('results/c.png')
             draw()
             
@@ -375,22 +485,46 @@ def main():
         'rho_star':0,
         'D':0}
 
+
+    # control variables
     simulation_length = 10000
-    N_E = 400
+    stair_length = 1000
+    resets = simulation_length / stair_length
+
+    N_E = 500
     N_I = 1
-    sample = 15
-
-    I_ext_E = build_input([0,1,0,1], [0, 0.25,0.5,0.75, 1], simulation_length, N_E)
-    I_ext_I = build_input([0,0,0,0], [0, 0.25,0.5,0.75, 1], simulation_length, N_I)
-
-    stair_length = 50
+    sample = 3
+    
+    # input current candidates
     I_ext_E_increasing = build_increasing_input(0, 40, stair_length, simulation_length, N_E)
-    #print I_ext_E_increasing
-    #I_ext_E = 25*np.ones((simulation_length, N_E), dtype=float64)
-    #I_ext_I = 25*np.ones((simulation_length, N_I), dtype=float64)
+    I_ext_I_increasing = build_increasing_input(0, 40, stair_length, simulation_length, N_I)
+    I_ext_E_stable = build_input([12], [0,1], simulation_length, N_E)
+    I_ext_I_stable = build_input([12], [0,1], simulation_length, N_I)
+    I_ext_E_4_phase = build_input([0,1,0,1], [0, 0.25,0.5,0.75, 1], simulation_length, N_E)
+    I_ext_I_4_phase = build_input([0,0,0,0], [0, 0.25,0.5,0.75, 1], simulation_length, N_I)
+
+    input_flag = 'stair'
+    #input_flag = 'stable'
+    #input_flag = '4_phase'
+    
+
+    if input_flag == 'stair':
+        I_ext_E = I_ext_E_increasing
+        I_ext_I = I_ext_I_increasing
+    elif input_flag == 'stable':
+        I_ext_E = I_ext_E_stable
+        I_ext_I = I_ext_I_stable
+        resets = 1
+    elif input_flag == '4_phase':
+        I_ext_E = I_ext_E_4_phase
+        I_ext_I = I_ext_I_4_phase
 
     debug = False
-    param_trial_num = 2
+    mode = 'cpp_standalone'
+    param_trial_num = 1
+
+
+    # result variables
     binned_rate_E = np.zeros((simulation_length * 10, param_trial_num))
     binned_rate_I = np.zeros((simulation_length * 10, param_trial_num))
     rho = np.zeros((sample, simulation_length, param_trial_num))
@@ -401,14 +535,23 @@ def main():
     '''
 
 
+
+    t = arange(simulation_length)
+    
     sim = Brian_Simulator(simulation_length=simulation_length, N_E=N_E,N_I=N_I,sample=sample,
-            I_ext_E=I_ext_E_increasing, I_ext_I=I_ext_I, params=params, debug=debug)
+            I_ext_E=I_ext_E, I_ext_I=I_ext_I, params=params, debug=debug)
     for i in arange(param_trial_num):
+        cpp_directory = 'output_'+str(i)
         param_diffs['sigma'] = 5*(i+1)
         #params['sigma'] = i * 5
-        (binned_rate_E[:,i], binned_rate_I[:,i], rho[:,:,i]) = sim.run(param_diffs)
+        (binned_rate_E[:,i], binned_rate_I[:,i], rho[:,:,i]) = sim.run(param_diffs, mode=mode, resets=resets, cpp_directory=cpp_directory)
+        print 't ', t.shape
+        #call(['rm','-r',cpp_directory])
 
-    visualize_IF_curve(I_ext_E_increasing, binned_rate_E)
+    visualize_all(I_ext_E, binned_rate_E, rho,t,  resets)
+    #visualize_tI_curve(I_ext_E_stable, t)
+    #visualize_IF_curve(I_ext_E_stable, binned_rate_E, t)
+    #visualize_F_rho_curve(binned_rate_E, rho,I_ext_E_stable,t, resets)
     show()
 
 
